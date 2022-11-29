8ed74595023c3f0558e2ca2646f6d878743f2a67
Omar Montoya <alfredo.montoya@cesunbc.edu.mx>
2022-11-29 08:04:54 -0800

    Create Simple Test for Helpers

--- src/__test__/helpers/index.test.js
+++ src/__test__/helpers/index.test.js
@@ -1,7 +1,14 @@
-import { countVowels } from '../../helpers';
+import { countVowels, countConsonants } from '../../helpers';
 
 describe('Test all helpers functions', () => {
+    // countVowels
     test('Returns a valid count of vowels', () => {
         expect(countVowels('Omar')).toBe(2);
-    })
+        expect(countConsonants('Omar')).not.toBe(3);
+    });
+    // countConsonants
+    test('Returns a calid count of consonants', () => {
+        expect(countConsonants('Omar')).toBe(2);
+        expect(countConsonants('Omar')).not.toBe(3);
+    });
 });
