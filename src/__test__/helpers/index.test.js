1dccf8599c638298dba71475a696513afc649363
Omar Montoya <alfredo.montoya@cesunbc.edu.mx>
2022-11-29 10:52:23 -0800

    Added isOdd tests

--- src/__test__/helpers/index.test.js
+++ src/__test__/helpers/index.test.js
@@ -1,4 +1,4 @@
-import { countVowels, countConsonants, isConsonant } from '../../helpers';
+import { countVowels, countConsonants, isConsonant, isOdd } from '../../helpers';
 
 describe('Test all helpers functions', () => {
     // countVowels
@@ -15,5 +15,10 @@ describe('Test all helpers functions', () => {
     test('Returns a true/false based on given consonant', () => {
         expect(isConsonant('M')).toBeTruthy();
         expect(isConsonant('A')).toBeFalsy();
+    });
+    // isOdd
+    test('Returns a true/false based on given value', () => {
+        expect(isOdd(4)).toBeTruthy();
+        expect(isOdd(1)).toBeFalsy();
     });
 });
