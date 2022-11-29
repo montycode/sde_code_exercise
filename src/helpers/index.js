65bd6f5e30edb8c71314f1ad2c9420224b95f74c
Omar Montoya <alfredo.montoya@cesunbc.edu.mx>
2022-11-29 10:51:23 -0800

    Added isOdd Callback

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,4 @@
 export { default as countVowels } from './countVowels';
 export { default as countConsonants } from './countConsonants';
 export { default as isConsonant } from './isConsonant';
+export { default as isOdd } from './isOdd';
---
+++ src/helpers/isOdd.js
@@ -0,0 +1,8 @@
+// Helper to see if shipment's destination street name is odd or even.
+// Not really necesary but helps code readability.
+
+const isOdd = (number) => {
+     return !(number % 2);
+};
+
+export default isOdd;
