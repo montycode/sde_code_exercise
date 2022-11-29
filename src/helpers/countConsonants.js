070d3eccc80f7a9f2867d0583d5f56eae0821dea
Omar Montoya <alfredo.montoya@cesunbc.edu.mx>
2022-11-29 10:52:43 -0800

    Fix import for module solving

--- src/helpers/countConsonants.js
+++ src/helpers/countConsonants.js
@@ -1,7 +1,7 @@
 // (SS) is the number of consonants in the driver’s name multiplied by 1.
 // Next I'll create a callback to count vowels
 
-import isConsonant from "./isConsonant";
+import isConsonant from "./isConsonant.js";
 
 const countConsonants = (driverName) => {
     let count = 0;
